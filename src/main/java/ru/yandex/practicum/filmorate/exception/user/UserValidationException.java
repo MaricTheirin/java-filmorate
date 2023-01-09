7d44d35e8e15e0616402e02f8e8395276c01cb69
MaricTheirin <ghostmeee@yandex.ru>
2023-01-09 21:46:55 +0300

    refactor: Изменить коды ответа сервера в случае ошибок

--- src/main/java/ru/yandex/practicum/filmorate/exception/user/UserValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/user/UserValidationException.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.filmorate.exception.user;
 
+import org.springframework.http.HttpStatus;
 import ru.yandex.practicum.filmorate.exception.ControllerException;
 
 public class UserValidationException extends ControllerException {
     public UserValidationException(String message) {
         super(message);
+        responseStatus = HttpStatus.BAD_REQUEST;
     }
 }
