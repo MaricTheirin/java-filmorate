5c4a1b725058e58bccffd531928d140c9ab1b29b
MaricTheirin <ghostmeee@yandex.ru>
2023-01-18 11:07:51 +0300

    fix - Исправить наименование компоненты DbMpaStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/DbMpaStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/DbMpaStorage.java
@@ -11,7 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-@Component("dbMpaRatingStorage")
+@Component("dbMpaStorage")
 public class DbMpaStorage implements MpaStorage {
 
     private final JdbcTemplate template;
